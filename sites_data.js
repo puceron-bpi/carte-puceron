17f52bc443572e8a0d3f27b8f197dccf9d7d3f32
puceron-bpi <v.mathieu@plantsdebretagne.com>
2026-04-30 11:15:26 +0200

    🔄 Mise à jour automatique de sites_data.js via script

--- sites_data.js
+++ sites_data.js
@@ -42,78 +42,6 @@ const sites = [
     "myzusColor": "#ffffff"
   },
   {
-    "name": "St caradec",
-    "coords": [
-      48.1913,
-      -2.8503
-    ],
-    "color": "#4daf4a",
-    "data": [],
-    "myzusDate": null,
-    "myzusTaux": null,
-    "myzusColor": "#ffffff"
-  },
-  {
-    "name": "Plougar",
-    "coords": [
-      48.563,
-      -4.1411
-    ],
-    "color": "#984ea3",
-    "data": [],
-    "myzusDate": null,
-    "myzusTaux": null,
-    "myzusColor": "#ffffff"
-  },
-  {
-    "name": "Plabennec",
-    "coords": [
-      48.4989,
-      -4.4185
-    ],
-    "color": "#ff7f00",
-    "data": [],
-    "myzusDate": null,
-    "myzusTaux": null,
-    "myzusColor": "#ffffff"
-  },
-  {
-    "name": "Ploeuc",
-    "coords": [
-      48.35,
-      -2.75
-    ],
-    "color": "#ffff33",
-    "data": [],
-    "myzusDate": null,
-    "myzusTaux": null,
-    "myzusColor": "#ffffff"
-  },
-  {
-    "name": "St Gilles les bois",
-    "coords": [
-      48.6527,
-      -3.1054
-    ],
-    "color": "#a65628",
-    "data": [],
-    "myzusDate": null,
-    "myzusTaux": null,
-    "myzusColor": "#ffffff"
-  },
-  {
-    "name": "Guiclan",
-    "coords": [
-      48.5511,
-      -3.9623
-    ],
-    "color": "#f781bf",
-    "data": [],
-    "myzusDate": null,
-    "myzusTaux": null,
-    "myzusColor": "#ffffff"
-  },
-  {
     "name": "Hanvec",
     "coords": [
       48.3275,
@@ -138,30 +66,6 @@ const sites = [
         "count": 13
       }
     ],
-    "myzusDate": null,
-    "myzusTaux": null,
-    "myzusColor": "#ffffff"
-  },
-  {
-    "name": "Chateauneuf",
-    "coords": [
-      48.1876,
-      -3.8179
-    ],
-    "color": "#66c2a5",
-    "data": [],
-    "myzusDate": null,
-    "myzusTaux": null,
-    "myzusColor": "#ffffff"
-  },
-  {
-    "name": "Plogonnec",
-    "coords": [
-      48.0781,
-      -4.1916
-    ],
-    "color": "#fc8d62",
-    "data": [],
     "myzusDate": null,
     "myzusTaux": null,
     "myzusColor": "#ffffff"
