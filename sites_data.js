ffb4cf3ddfc249094b91c5ef46b54ce615aa1722
puceron-bpi <v.mathieu@plantsdebretagne.com>
2026-05-05 14:52:49 +0200

    🔄 Mise à jour automatique de sites_data.js via script

--- sites_data.js
+++ sites_data.js
@@ -1,54 +1,24 @@
 const sites = [
   {
-    "name": "Bieuzy",
-    "coords": [
-      47.9829,
-      -3.0639
-    ],
-    "color": "#e41a1c",
-    "data": [
-      {
-        "date": "2026-04-27",
-        "count": 45
-      },
-      {
-        "date": "2026-04-28",
-        "count": 55
-      },
-      {
-        "date": "2026-04-29",
-        "count": 64
-      }
-    ],
-    "myzusDate": null,
-    "myzusTaux": null,
-    "myzusColor": "#ffffff"
-  },
-  {
-    "name": "Cléguérec",
-    "coords": [
-      48.1246,
-      -3.0694
-    ],
-    "color": "#377eb8",
-    "data": [
-      {
-        "date": "2026-04-27",
-        "count": 45
-      }
-    ],
-    "myzusDate": null,
-    "myzusTaux": null,
-    "myzusColor": "#ffffff"
-  },
-  {
     "name": "Hanvec",
     "coords": [
       48.3275,
       -4.1586
     ],
-    "color": "#999999",
+    "color": "#e41a1c",
     "data": [
+      {
+        "date": "2026-04-24",
+        "count": 10
+      },
+      {
+        "date": "2026-04-25",
+        "count": 10
+      },
+      {
+        "date": "2026-04-26",
+        "count": 10
+      },
       {
         "date": "2026-04-27",
         "count": 10
@@ -64,6 +34,226 @@ const sites = [
       {
         "date": "2026-04-30",
         "count": 13
+      },
+      {
+        "date": "2026-05-01",
+        "count": 20
+      },
+      {
+        "date": "2026-05-02",
+        "count": 28
+      },
+      {
+        "date": "2026-05-03",
+        "count": 35
+      },
+      {
+        "date": "2026-05-04",
+        "count": 43
+      }
+    ],
+    "myzusDate": null,
+    "myzusTaux": null,
+    "myzusColor": "#ffffff"
+  },
+  {
+    "name": "Bieuzy",
+    "coords": [
+      47.9829,
+      -3.0639
+    ],
+    "color": "#377eb8",
+    "data": [
+      {
+        "date": "2026-04-21",
+        "count": 45
+      },
+      {
+        "date": "2026-04-22",
+        "count": 45
+      },
+      {
+        "date": "2026-04-23",
+        "count": 45
+      },
+      {
+        "date": "2026-04-24",
+        "count": 45
+      },
+      {
+        "date": "2026-04-25",
+        "count": 45
+      },
+      {
+        "date": "2026-04-26",
+        "count": 45
+      },
+      {
+        "date": "2026-04-27",
+        "count": 45
+      },
+      {
+        "date": "2026-04-28",
+        "count": 55
+      },
+      {
+        "date": "2026-04-29",
+        "count": 64
+      },
+      {
+        "date": "2026-04-30",
+        "count": 64
+      },
+      {
+        "date": "2026-05-01",
+        "count": 63
+      },
+      {
+        "date": "2026-05-02",
+        "count": 62
+      },
+      {
+        "date": "2026-05-03",
+        "count": 62
+      },
+      {
+        "date": "2026-05-04",
+        "count": 61
+      }
+    ],
+    "myzusDate": null,
+    "myzusTaux": null,
+    "myzusColor": "#ffffff"
+  },
+  {
+    "name": "Cléguérec",
+    "coords": [
+      48.1246,
+      -3.0694
+    ],
+    "color": "#4daf4a",
+    "data": [
+      {
+        "date": "2026-04-27",
+        "count": 28
+      },
+      {
+        "date": "2026-04-28",
+        "count": 28
+      },
+      {
+        "date": "2026-04-29",
+        "count": 28
+      },
+      {
+        "date": "2026-04-30",
+        "count": 95
+      },
+      {
+        "date": "2026-05-01",
+        "count": 161
+      },
+      {
+        "date": "2026-05-02",
+        "count": 227
+      },
+      {
+        "date": "2026-05-03",
+        "count": 294
+      },
+      {
+        "date": "2026-05-04",
+        "count": 360
+      }
+    ],
+    "myzusDate": null,
+    "myzusTaux": null,
+    "myzusColor": "#ffffff"
+  },
+  {
+    "name": "Plogonnec",
+    "coords": [
+      48.0781,
+      -4.1916
+    ],
+    "color": "#984ea3",
+    "data": [
+      {
+        "date": "2026-04-27",
+        "count": 105
+      },
+      {
+        "date": "2026-04-28",
+        "count": 105
+      },
+      {
+        "date": "2026-04-29",
+        "count": 105
+      },
+      {
+        "date": "2026-04-30",
+        "count": 105
+      },
+      {
+        "date": "2026-05-01",
+        "count": 96
+      },
+      {
+        "date": "2026-05-02",
+        "count": 88
+      },
+      {
+        "date": "2026-05-03",
+        "count": 79
+      },
+      {
+        "date": "2026-05-04",
+        "count": 70
+      }
+    ],
+    "myzusDate": null,
+    "myzusTaux": null,
+    "myzusColor": "#ffffff"
+  },
+  {
+    "name": "Guiclan",
+    "coords": [
+      48.5511,
+      -3.9623
+    ],
+    "color": "#ffff33",
+    "data": [
+      {
+        "date": "2026-04-27",
+        "count": 7
+      },
+      {
+        "date": "2026-04-28",
+        "count": 7
+      },
+      {
+        "date": "2026-04-29",
+        "count": 7
+      },
+      {
+        "date": "2026-04-30",
+        "count": 7
+      },
+      {
+        "date": "2026-05-01",
+        "count": 17
+      },
+      {
+        "date": "2026-05-02",
+        "count": 27
+      },
+      {
+        "date": "2026-05-03",
+        "count": 38
+      },
+      {
+        "date": "2026-05-04",
+        "count": 48
       }
     ],
     "myzusDate": null,
